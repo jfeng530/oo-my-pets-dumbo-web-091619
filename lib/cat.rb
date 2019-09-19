f77b9bc43a092e411157836df25efa17bee8404e
Jacky Feng <jackyfeng530@yahoo.com>
2019-09-19 20:51:39 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,8 +1,17 @@
 class Cat
   
+  attr_reader :name, :owner
+  @@all = []
+  
   def initialize(name, owner)
     @name = name
     @owner = owner
+    @mood = "nervous"
+    @@all << self 
   end
   
+  def self.all 
+    @@all
+  end
+
 end
